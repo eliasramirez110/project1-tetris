1a1936eae42cffc1774981ec24b3b90fb3b32e62
Elias Ramirez <ramirezelias110@gmail.com>
2021-03-19 19:01:04 -0400

    friday commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const grid = document.querySelector('.grid') 
+let squares = Array.from(document.querySelectorAll('.grid div'))
+const ScoreDisplay = document.querySelector('#score')
+const startBtn = document.querySelector('#start-button')
+const width = 10
+
+
+console.log(squares)
+
+console.log(ScoreDisplay)
+
+  
+// // const tetrominoes = [ltetromino, zTetromino, oTetromino, iTetromino, tTetromino]
+
