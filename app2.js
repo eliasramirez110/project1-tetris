9d24f2f8226e4e8801fc7ec5a5ea6a817461490a
Elias Ramirez <ramirezelias110@gmail.com>
2021-03-24 00:54:04 -0400

    project

--- app2.js
+++ app2.js
@@ -1,206 +1,265 @@
-//grabbing and creating the canvas
-const cvs = document.getElementById("tetris");
-const ctx = cvs.getContext("2d");
-const scoreElement = document.getElementById("score");
+const cvs = document.getElementById("canvas")
+const ctx = cvs.getContext("2d")
+const scoreElement = document.getElementById("score")
 
-//grid size and square size
 const row = 20
 const col = 10
 const square = 20
-const emptySquare = "white"
+const emptySquare = "white" // color of an empty square
 
-// function to draw a square
-  drawSquare = (x,y,color) => {
+// draw a square
+drawSquare = (x,y,color) => {
     ctx.fillStyle = color
-    ctx.fillRect(x*square, y*square, square, square)
-    ctx.strokeStyle = "BLACK"
-    ctx.strokeRect(x*square, y*square, square, square) 
+    ctx.fillRect(x*square,y*square,square,square)
+
+    ctx.strokeStyle = "gray"
+    ctx.strokeRect(x*square,y*square,square,square)
 }
 
-// create the board. the board is an array so I use for loop to make the rows and columns
-let board = [];
-for(r = 0; r < row; r++){
-    board[r] = [];
+// create the game board
+let board = []
+for( r = 0; r < row; r++){
+    board[r] = []
     for(c = 0; c < col; c++){
-        board[r][c] = emptySquare;
+        board[r][c] = emptySquare
     }
 }
 
-// draw the board.
+// draw the board
 drawBoard = () => {
-    for(r = 0; r < row; r++){
+    for( r = 0; r < row; r++){
         for(c = 0; c < col; c++){
-            drawSquare(c,r,board[r][c]);
+            drawSquare(c,r,board[r][c])
         }
     }
 }
-drawBoard();
+drawBoard()
 
-
-
-//Shapes and their colors
-const Z = [
-  [
-    [1,1,0]
-    [0,1,1]
-    [0,0,0]
-  ]
-  
-  [ 
-    [0,0,1]
-    [0,1,1]
-    [0,1,0]
-  ]
-  
-  [
-    [0,0,0]
-    [1,1,0]
-    [0,1,1]
-  ]
-
-  [
-   [0,1,0]
-   [1,1,0]
-   [1,0,0] 
-  ]
+// the pieces and their colors. pieces are hard coded in a separate file
+const pieces = [
+    [Z,"red"],
+    [S,"green"],
+    [T,"yellow"],
+    [O,"blue"],
+    [L,"purple"],
+    [I,"cyan"],
+    [J,"orange"]
 ]
 
-const shapes = [
-  // [I, "orange"],
-  // [O, "purple"],
-  // [J, "blue"],
-  // [L, "green"],
-  // [S, "brown"],
-  // [T, "red"],
-  [Z, "aqua"]
-]
-
-//initate a piece
-
-// let shapes = new Shape(tetroShape[0][0], tetroShape[1])
-
-
-//The objects for the shapes
-shape = (tetromino, color) => {
-  this.tetromino = tetromino
-  this.color = color
-
-  this.tetrominoN = 0 
-  this.activeTetromino = this.tetromino[this.tetrominoN]
-
-  //to control/move the shapes
-  this.x = 3
-  this.y = -2
+// generate random pieces
+randomPiece = () => {
+    let r = randomN = Math.floor(Math.random() * pieces.length) 
+    return new Piece( pieces[r][0],pieces[r][1])
 }
 
-//to generate random shapes
-// randomPiece = () => {
-//   let r = randomN = Math.floor(Math.random() * shapes.length) 
-//   return new Piece( PIECES[r][0],PIECES[r][1]);
-// }
+let p = randomPiece()
 
-//fill function
-// Shape.prototype.fill = () => {
-//   for(r = 0; r < this.activeTetromino.length; r++){
-//     for(c = 0; c < this.activeTetromino.length; c++){
-//       //to draw only occupied squares  
-//       if(this.activeTetromino[r][c]){
-//         drawSquare(this.x + c, this.y + r, color)
-//         }
-//       }
-//   }
-// }
-
-// to draw and undraw 
-shape.prototype.draw = () => {
-  this.fill(this.color)
+// The object for the pieces. 
+function Piece(tetromino,color) {
+    this.name = tetromino
+    this.tetromino = tetromino
+    this.color = color
+    
+    this.tetrominoN = 0 //we start from the first pattern
+    this.activeTetromino = this.tetromino[this.tetrominoN];
+    
+    // this is where the pieces come from on the canvas
+    this.x = 3;
+    this.y = -2;
 }
 
-shape.prototype.undraw = () => {
-  this.fill(emptySquare)
-}
+// fill function
 
-
-
-//to draw a shape on the board
-shape.prototype.draw = () => {
-  for(r = 0; r < this.activeTetromino.length; r++){
-    for(c = 0; c < this.activeTetromino.length; c++){
-      //to draw only occupied squares  
-      if(this.activeTetromino[r][c]){
-        drawSquare(this.x + c, this.y + r, this.color)
+Piece.prototype.fill = (color) => {
+    for( r = 0; r < this.activeTetromino.length; r++){
+        for(c = 0; c < this.activeTetromino.length; c++){
+            // we draw only occupied squares
+            if( this.activeTetromino[r][c]){
+                drawSquare(this.x + c,this.y + r, color)
+            }
         }
-      }
-  }
+    }
 }
 
-//undraw a shape. this is the same function as the draw, instead of this.color I used the emptySquare 
-shape.prototype.draw = () => {
-  for(r = 0; r < this.activeTetromino.length; r++){
-    for(c = 0; c < this.activeTetromino.length; c++){
-      //to draw only occupied squares  
-      if(this.activeTetromino[r][c]){
-        drawSquare(this.x + c, this.y + r, emptySquare)
+// draw a piece to the board
+
+  Piece.prototype.draw = () => {
+    this.fill(this.color)
+}
+
+// undraw a piece
+Piece.prototype.unDraw = () => {
+    this.fill(emptySquare)
+}
+
+// move Down the piece
+Piece.prototype.moveDown = () => {
+    if(!this.collision(0,1,this.activeTetromino)){
+        this.unDraw()
+        this.y++
+        this.draw()
+    }else{
+        // we lock the piece and generate a new one
+        this.lock()
+        p = randomPiece()
+    }
+    
+}
+
+// move Right the piece
+Piece.prototype.moveRight = () => {
+    if(!this.collision(1,0,this.activeTetromino)){
+        this.unDraw()
+        this.x++
+        this.draw()
+    }
+}
+
+// move Left the piece
+Piece.prototype.moveLeft = function(){
+    if(!this.collision(-1,0,this.activeTetromino)){
+        this.unDraw()
+        this.x--
+        this.draw()
+    }
+}
+
+// rotate the piece
+Piece.prototype.rotate = () => {
+    let nextPattern = this.tetromino[(this.tetrominoN + 1)%this.tetromino.length]
+    let block = 0
+    
+    if(this.collision(0,0,nextPattern)){
+        if(this.x > col/2){
+            // it's the right wall
+            kick = -1 // we need to move the piece to the left
+        }else {
+            // it's the left wall
+            kick = 1 // we need to move the piece to the right
         }
-      }
-  }
+    }
+    
+    if(!this.collision(kick,0,nextPattern)){
+        this.unDraw()
+        this.x += block
+        this.tetrominoN = (this.tetrominoN + 1)%this.tetromino.length 
+        this.activeTetromino = this.tetromino[this.tetrominoN]
+        this.draw()
+    }
 }
 
+let score = 0
 
-
-
-//move down shape
-shape.prototype.moveDown = () => {
-  this.undraw()//update made with undraw()
-  this.y++
-  this.draw()
-}
-
-//move right
-shape.prototype.moveRight = () => {
-  this.undraw()//update made with undraw()
-  this.x++
-  this.draw()
-}
-//move left
-shape.prototype.moveDown = () => {
-  this.undraw()//update made with undraw()
-  this.x--
-  this.draw()
-}
-
-//rotate
-shape.prototype.rotate = () => {
-  this.undraw()//update made with undraw()
-  this.tetrominoN = (this.tetrominoN + 1)%this.tetromino.length
-  this.activeTetromino = 
-  this.draw()
-}
-
-//to control the shapes
-document.addEventListener("keydown", control)
-control = (event) => {
-  if(event.keyCode === 37){
-    shp.moveLeft()
-  } else if(event.keyCode === 38) {
-    shp.rotate()
-  } else if(event.keyCode === 39) {
-    shp.moveRight()
-  } else if (event.keyCode === 40) {
-    shp.moveDown
-  }
-}
-
-
-//undraw a shape. this is the same function as the draw, we just use diff color
-shape.prototype.draw = () => {
-  for(r = 0; r < this.activeTetromino.length; r++){
-    for(c = 0; c < this.activeTetromino.length; c++){
-      //to draw only occupied squares  
-      if(this.activeTetromino[r][c]){
-        drawSquare(this.x + c, this.y + r, emptySquare)
+Piece.prototype.lock = () => {
+    for( r = 0; r < this.activeTetromino.length; r++){
+        for(c = 0; c < this.activeTetromino.length; c++){
+            // we skip the vacant squares
+            if( !this.activeTetromino[r][c]){
+                continue
+            }
+            // pieces to lock on top = game over
+            if(this.y + r < 0){
+                alert("Game Over")
+                // stop request animation frame
+                gameOver = true
+                break
+            }
+            // we lock the piece
+            board[this.y+r][this.x+c] = this.color
         }
-      }
-  }
+    }
+    // remove full rows
+    for(r = 0; r < row; r++){
+        let isRowFull = true
+        for( c = 0; c < col; c++){
+            isRowFull = isRowFull && (board[r][c] != emptySquare)
+        }
+        if(isRowFull){
+            // if the row is full
+            // we move down all the rows above it
+            for( y = r; y > 1; y--){
+                for( c = 0; c < col; c++){
+                    board[y][c] = board[y-1][c]
+                }
+            }
+            // the top row board[0][..] has no row above it
+            for( c = 0; c < col; c++){
+                board[0][c] = emptySquare
+            }
+            // increment the score
+            score += 1
+        }
+    }
+    // update the board
+    drawBoard()
+    
+    // update the score
+    scoreElement.innerHTML = score
 }
 
+// collision fucntion
+
+Piece.prototype.collision = (x,y,piece) => {
+    for( r = 0; r < piece.length; r++){
+        for(c = 0; c < piece.length; c++){
+            // if the square is empty, we skip it
+            if(!piece[r][c]){
+                continue;
+            }
+            // coordinates of the piece after movement
+            let newX = this.x + c + x
+            let newY = this.y + r + y
+            
+            // conditions
+            if(newX < 0 || newX >= col || newY >= ROW){
+                return true;
+            }
+            // skip newY < 0; board[-1] will crush our game
+            if(newY < 0){
+                continue;
+            }
+            // check if there is a locked piece alrady in place
+            if( board[newY][newX] != emptySquare){
+                return true
+            }
+        }
+    }
+    return false
+}
+
+// CONTROL the piece
+
+document.addEventListener("keydown",control)
+
+function control(event) {
+    if(event.keyCode == 37){
+        p.moveLeft()
+        dropStart = Date.now()
+    }else if(event.keyCode == 38){
+        p.rotate();
+        dropStart = Date.now()
+    }else if(event.keyCode == 39){
+        p.moveRight();
+        dropStart = Date.now()
+    }else if(event.keyCode == 40){
+        p.moveDown()
+    }
+}
+
+// drop the piece every 1sec
+
+let dropStart = Date.now()
+let gameOver = false
+drop = () => {
+    let now = Date.now()
+    let delta = now - dropStart
+    if(delta > 1000){
+        p.moveDown()
+        dropStart = Date.now()
+    }
+    if( !gameOver){
+        requestAnimationFrame(drop)
+    }
+}
+
+drop()
